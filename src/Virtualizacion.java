decb1f6d2200a633c962f9e8e691b805920c5744
ivangonzalez <gonzalezcastroivanpatricio@gmail.com>
2022-10-21 17:19:56 -0500

    Creacion de una nueva clase llamada virtualizacion

---
+++ src/Virtualizacion.java
@@ -0,0 +1,7 @@
+public class Virtualizacion {
+    public static void main(String[] args) {
+        System.out.println("TEMAS DE PROYECTOS EXPLICADOS EN CENTRO DE CONVENCIONES ");
+        System.out.println("Juego Educativo: ");
+        System.out.println();
+    }
+}
