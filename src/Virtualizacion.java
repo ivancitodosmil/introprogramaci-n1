40de615d13012a33811e88219f4c18f1413fee0e
ivangonzalez <gonzalezcastroivanpatricio@gmail.com>
2022-10-21 17:51:45 -0500

    Virtualización Charla

--- src/Virtualizacion.java
+++ src/Virtualizacion.java
@@ -1,7 +1,21 @@
 public class Virtualizacion {
     public static void main(String[] args) {
         System.out.println("TEMAS DE PROYECTOS EXPLICADOS EN CENTRO DE CONVENCIONES ");
-        System.out.println("Juego Educativo: ");
-        System.out.println();
+        System.out.println("Juego Educativo Para Que Los Niños Aprendan Figuras: ");
+        System.out.print("El primer y más llamativo proyecto a mi parecer trata sobre una aplicación para enseñar a los niños a ");
+        System.out.println( "identificar figuras por medio de un juego llamativo");
+
+        System.out.println("Otros Aplicativos");
+        System.out.println("Filtros de Instagram con temática en el jaguar de la institución UTPL");
+        System.out.println("Creación de dibujos animatrónicos para entretenimiento");
+        System.out.println("Creación de un juego llamado gallina Turuleca");
+        System.out.println("Aplicativo 360 para aprender tres idiomas");
+
+
+
+
+
+
+
     }
 }
