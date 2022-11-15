6f5b5e6e6bded6b9067b8d8cb7e936b11eeb4695
ivancitodosmil <116390060+ivancitodosmil@users.noreply.github.com>
2022-11-15 15:09:40 -0500

    Add files via upload

---
+++ src/Contenido.java
@@ -0,0 +1,30 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("       20 Conceptos Básicos De La programación       ");
+        System.out.println("Lenguaje de programación: Permite desarrollar programas para computadoras.");
+        System.out.println("Programa fuente: Programa escrito en alguno de los lenguajes de programación.");
+        System.out.println("Algotirmo: Secuencia de pasos finitos bien definidos que resuelven un problema.");
+        System.out.println("Programa objeto: Programa que se encuentra en lenguaje máquina y que ya es ejecutable por ésta.");
+        System.out.println("Compilador: Traduce un programa de lenguaje de programación de alto nivel en lenguaje de máquina.");
+        System.out.println("Pseudocódigo: Forma de expresar los distintos pasos que va a realizar un programa.");
+        System.out.println("Diagramas de flujo: Esquematización gráfica de un algoritmo.");
+        System.out.println("Comentarios: Los comentarios son anotaciones legibles y aclaratorias.");
+        System.out.println("Bug: Problema en el código que hace que no ejecute correctamente su función.");
+        System.out.println("Backdoor: Puerto creado por alguna aplicación maliciosa.");
+        System.out.println("Backup: Copia de archivos en otro dispositivo para guardar información.");
+        System.out.println("Big Data: almacenamiento de grandes cantidades de datos.");
+        System.out.println("Terminal: es una Interfaz de Línea de Comandos.");
+        System.out.println("Plugin: Un extensión de código que se puede agregar a un código en el que estás trabajando.");
+        System.out.println("Interpretador: Programas de la computadora que leen un código fuente.");
+        System.out.println("Lenguaje de máquina: lenguaje con el nivel más bajo de comprensión para el ser humano.");
+        System.out.println("Código: conjunto de palabras o símbolos que contienen instrucciones para la computadora");
+        System.out.println("Console: ventana, normalmente negra, donde se puede trabajar desde la línea de comandos.");
+        System.out.println("Shell: ventana, normalmente negra, para manipular el sistema operativo a través de la línea de comandos.");
+        System.out.println("Sprint: ciclo de tiempo dentro del cual se lleva a cabo un conjunto de actividades.");
+
+
+
+    }
+
+
+}
---
+++ src/Git.java
@@ -0,0 +1,21 @@
+public class Git {
+    public class Git {
+        public static void main(String[] args) {
+            System.out.println("Comandos Git Utilizados Desde La Inicialización Del Proyecto a  Git Hasta La Colocación De Los Archivos en Github");
+
+            System.out.println("git --global user.email gonzalezcastroivanpatricio@gmail.com: Permite introducir nuestro correo puesto Git ");
+            System.out.println("git config --global user.name ivangonzalez: Permite colocar nuestro seudónimo ");
+            System.out.println("git init: Nos permite iniciar la acción para guardar en Git");
+            System.out.println("git add . : Adjunta el proceso realizado");
+            System.out.println(" git add .\.git\ : Nos permite proceder a iniciar para adjuntar en git");
+            System.out.println("git status : Para verificar el estado del archivo ha subir en Git");
+            System.out.println("git commit -m claseprogramacion : Captura una instantánea de los cambios preparados en ese momento del proyecto ");
+            System.out.println("git remote add origin : Te permite crear, ver y eliminar conexiones con otros repositorios ");
+            System.out.println("git branch -M main: Una especie de herramienta de gestión de ramas ");
+            System.out.println("git push -u origin main : Permite insertar los cambios y programas realizados a Git ");
+
+
+
+
+
+        }
